565994492ee208698dafda40763805fc165509cc
JeongGweonHo <jkho1229@naver.com>
2017-05-29 04:33:46 +0900

    final

--- sw/blog/views.py
+++ sw/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 def index(request):
@@ -6,5 +6,11 @@ def index(request):
 	return render(request, 'blog/post_list.html', {
 		'post_list' : post_list,
 	})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {
+			'post' : post,
+		})
 
 # Create your views here.
